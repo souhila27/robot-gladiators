0247a666f92cf2feda6d5d2d6771761ff671b24c
souhila <suhamimisou14@gmail.com>
2021-11-25 14:02:20 -0500

    add function on js

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+//this create a function named "fight"
+function fight() {
+    window.alert("the fight has begun!");
+    var playerName = window.prompt("what is your name?");
+   var playerName = " Tony the Robot";
+//note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("our robot's name is" + playerName);
+}
+
+fight();
+
